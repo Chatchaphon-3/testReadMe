380eaedd4d94b749573b85c1f1ff66004f87cbcc
Chatchaphon-3 <chatchaphon2547@gmail.com>
2026-01-04 05:05:10 +0700

    deovev

--- prisma/migrations/20251229130245_make_id_in_bet_an_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Bet" ADD CONSTRAINT "Bet_pkey" PRIMARY KEY ("id");
--- src/config/oauth.ts
+++
@@ -1,4 +0,0 @@
-import { OAuth2Client } from "google-auth-library";
-import { env } from "./env.js";
-
-export const oauth = new OAuth2Client(env.GOOGLE_CLIENT_ID);
